69ea5b3717cf82223a1f714c6c5ccdefe47daa12
Federico Palmieri <fedepalmie95@gmail.com>
2021-10-21 01:13:58 -0300

    changes in async_await

--- async_await.js
+++ async_await.js
@@ -1,19 +1,19 @@
 const { destroyer } = require('./creatortxt');
 
-async function esperar2segundos() {
+async function esperar4segundos() {
   console.log(3);
   destroyer('i destroy this function');
   return await new Promise((resolve) => {
     setTimeout(() => {
       console.log(4);
       resolve();
-    }, 2000);
+    }, 4000);
   });
 }
 
 async function ejecutarMensajes() {
   console.log(2);
-  await esperar2segundos();
+  await esperar4segundos();
   console.log(5);
 }
 
