0e0c05fc9e3d8bcdd1d92492d76303648c7b5cf3
Federico Palmieri <fedepalmie95@gmail.com>
2021-10-21 01:32:56 -0300

    changes console log to English

--- readertxt.js
+++ readertxt.js
@@ -5,16 +5,16 @@ const streamRead = fs.createReadStream('./file.txt', { encoding: 'utf8' });
 function reader() {
   streamRead
     .on('open', () => {
-      console.log('abriendo archivo');
+      console.log('open file');
     })
     .on('data', () => {
       console.log('----');
     })
     .on('close', () => {
-      console.log('archivo cerrado');
+      console.log('file closed');
     })
     .on('error', () => {
-      console.log('error en el archivo');
+      console.log('error in the file');
     });
 }
 
